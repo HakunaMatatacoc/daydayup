d1fcc199605d8ca2442be3e1fc3947d23dc4bb02
jian.li <jian.li@joowing.com>
2020-04-07 13:31:31 +0800

    add serilaization

---
+++ src/com/serialization/SerializeDemo.java
@@ -0,0 +1,27 @@
+package com.serialization;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+public class SerializeDemo {
+    public static void main(String[] args) {
+        Employee e = new Employee();
+        e.name = "Reyan Ali";
+        e.address = "Phokka Kuan, Ambehta Peer";
+        e.SSN = 11122333;
+        e.number = 101;
+        try {
+            FileOutputStream fileOut =
+                    new FileOutputStream("/Users/lijian/Documents/employee.ser");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(e);
+            out.close();
+            fileOut.close();
+            System.out.printf("Serialized data is saved in /Users/lijian/Documents/employee.ser");
+        } catch (IOException i) {
+            i.printStackTrace();
+        }
+    }
+}
