b79adba9de41d2e888e6fef23ad914a7d7096def
jian.li <jian.li@joowing.com>
2020-07-02 15:05:25 +0800

    Update

---
+++ test/com/jianli/util/optional/Godness.java
@@ -0,0 +1,40 @@
+package com.jianli.util.optional;
+
+import java.util.Objects;
+
+public class Godness {
+
+    private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Godness godness = (Godness) o;
+        return Objects.equals(name, godness.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Godness(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Godness{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
